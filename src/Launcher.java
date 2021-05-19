f13846899cc0b3e6bc6b56632c7ebd248734a2de
Ashiu <clement.milisavljevic@epita.fr>
2021-05-19 11:04:26 +0200

    Loop

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Launcher {
+import java.util.Scanner;
+
+public class Launcher
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello!");
+        Scanner scan =  new Scanner(System.in);
+        while (true)
+        {
+            String command = scan.next();
+            if (command.equals("quit"))
+                break;
+            System.out.println("Unknown command");
+        }
+    }
 }
