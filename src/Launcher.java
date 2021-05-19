6bc11885640e69d7683d8e4947c9407f7d67835e
Ashiu <clement.milisavljevic@epita.fr>
2021-05-19 11:13:47 +0200

    fibo

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,6 +2,23 @@ import java.util.Scanner;
 
 public class Launcher
 {
+    public static int fibo(int n)
+    {
+        if (n <= 1)
+            return n;
+
+        int a = 0;
+        int b = 1;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+
     public static void main(String[] args)
     {
         System.out.println("Hello!");
@@ -11,7 +28,14 @@ public class Launcher
             String command = scan.next();
             if (command.equals("quit"))
                 break;
-            System.out.println("Unknown command");
+            else if (command.equals("fibo"))
+            {
+                int n = scan.nextInt();
+                System.out.println(fibo(n));
+                scan.nextLine();
+            }
+            else
+                System.out.println("Unknown command");
         }
     }
 }
